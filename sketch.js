06cd7cb4b86820193cfda1360e662290ea6eb0d5
AdamSpannbauer <spannbaueradam@gmail.com>
2020-11-18 11:35:03 -0500

    add ellipse overlay

--- sketch.js
+++ sketch.js
@@ -15,6 +15,18 @@ let eigVals;
 let eigVectors;
 let projectionMatrix;
 
+// to be actual p5js vectors
+let eigVector1;
+let eigVector2;
+
+let deltaAngle;
+let currAngle = 0;
+const angleSpeed = Math.PI / 1000;
+let angleVel;
+
+let maxX;
+let maxY;
+
 function setup() {
   createCanvas(canvasW, canvasH);
 
@@ -24,17 +36,35 @@ function setup() {
   eigVals = matUtils.eigenvalues2by2(covarianceMatrix);
   eigVectors = matUtils.eigenvectors2by2(covarianceMatrix, eigVals);
 
+  eigVector1 = createVector(eigVectors[0][0], eigVectors[0][1]);
+  eigVector2 = createVector(eigVectors[1][0], eigVectors[1][1]);
+
+  if (eigVector1.x < 0) {
+    eigVector1.rotate(PI);
+  }
+
   projectionMatrix = [
-    [eigVectors[0][0], eigVectors[1][0]],
-    [eigVectors[0][1], eigVectors[1][1]],
+    [eigVector1.x, eigVector2.x],
+    [eigVector1.y, eigVector2.y],
   ];
 
+  deltaAngle = eigVector1.heading();
+  if (deltaAngle < 0) {
+    angleVel = angleSpeed;
+  } else {
+    angleVel = -angleSpeed;
+  }
+  currAngle = deltaAngle;
+
   projectedData = matUtils.matMul(centeredData, projectionMatrix);
+  maxX = projectedData.reduce((accum, curr) => max([accum, abs(curr[0])]), 0);
+  maxY = projectedData.reduce((accum, curr) => max([accum, abs(curr[1])]), 0);
+  maxX = map(maxX, -1.5, 1.5, -width / 2, width / 2);
+  maxY = map(maxY, -1.5, 1.5, -height / 2, height / 2);
 }
 
 let dataStep;
 const centeringFrames = 100;
-const rotatingFrames = 200;
 function draw() {
   background(200);
 
@@ -61,58 +91,25 @@ function draw() {
       end,
     );
     plotUtils.plot2d(dataStep);
-  } else if (frameCount <= centeringFrames + rotatingFrames) {
-    start = centeringFrames;
-    end = centeringFrames + rotatingFrames;
-
-    dataStep = plotUtils.animatePoints(
-      centeredData,
-      projectedData,
-      frameCount,
-      start,
-      end,
-    );
-    plotUtils.plot2d(dataStep);
-
+  } else if (abs(currAngle) >= angleSpeed) {
     push();
+    rotate(currAngle);
+
+    plotUtils.plot2d(projectedData);
+
     strokeWeight(3);
     stroke(255, 0, 0);
-    line(
-      0, 0,
-      width * map(frameCount, start, end, eigVectors[0][0], 1),
-      width * map(frameCount, start, end, eigVectors[0][1], 0),
-    );
 
-    line(
-      0, 0,
-      -width * map(frameCount, start, end, eigVectors[0][0], 1),
-      -width * map(frameCount, start, end, eigVectors[0][1], 0),
-    );
-    stroke(150, 0, 0);
-    line(
-      0, 0,
-      height * map(frameCount, start, end, eigVectors[1][0], 0),
-      height * map(frameCount, start, end, eigVectors[1][1], 1),
-    );
-    line(
-      0, 0,
-      -height * map(frameCount, start, end, eigVectors[1][0], 0),
-      -height * map(frameCount, start, end, eigVectors[1][1], 1),
-    );
+    const diamX = maxX * 2.5;
+    const diamY = maxY * 2.5;
+    ellipse(0, 0, diamX, diamY);
+    line(-diamX / 2, 0, diamX / 2, 0);
+    line(0, -diamY / 2, 0, diamY / 2);
+
+    currAngle += angleVel;
     pop();
   } else {
-    dataStep = projectedData;
-    plotUtils.plot2d(dataStep);
-
-    push();
-    strokeWeight(3);
-    stroke(255, 0, 0);
-    line(0, 0, -width, 0);
-    line(0, 0, width, 0);
-    stroke(150, 0, 0);
-    line(0, 0, 0, -height);
-    line(0, 0, 0, height);
-    pop();
+    plotUtils.plot2d(projectedData);
   }
 
   fill(255);
